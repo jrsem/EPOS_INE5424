b96c7ec593c1a4e1d7363ff76e8dc7d169fba31b
jrsem <juniorsemerzier@gmail.com>
2019-06-22 08:37:54 -0400

    setup

--- src/machine/cortex_a/realview_pbx_init.cc
+++ src/machine/cortex_a/realview_pbx_init.cc
@@ -17,40 +17,14 @@ void RealView_PBX::pre_init()
 
 void RealView_PBX::init()
 {
-  Machine::enable_GIC();
-  Machine::enable_gic_processor_interface();
-  Machine::send_sgi(0x00, 0x0f, 0x01);
-  int *apAddr = (int *)0x10000030; // SYS_FLAGSSET register
-  *apAddr = (int)0x10000;          // all APs execute from 0x10000
-}
-
-void RealView_PBX::config_int(int intID, int targetCPU)
-{
-  int reg_offset, index, address;
-  char priority = 0x80;
-  reg_offset = (intID >> 3) & 0xFFFFFFFC;
-  index = intID & 0x1F;
-  address = (GIC_BASE + 0x1100) + reg_offset;
-  *(int *)address = (1 << index);
-  // set interrupt ID priority
-  reg_offset = (intID & 0xFFFFFFFC);
-  index = intID & 0x3;
-  address = (GIC_BASE + 0x1400) + reg_offset + index;
-  *(char *)address = (char)priority;
-  // set target CPUs
-  address = (GIC_BASE + 0x1800) + reg_offset + index;
-  *(char *)address = (char)(1 << targetCPU);
-}
-void RealView_PBX::config_gic()
-{
-  // set int priority mask register
-  *(int *)(GIC_BASE + 0x104) = 0xFF;
-  // Enable CPU interface control register to signal interrupts
-  *(int *)(GIC_BASE + 0x100) = 1;
-  // Enable distributor control register to send interrupts to CPUs
-  *(int *)(GIC_BASE + 0x1000) = 1;
-  config_int(36, 0); // timer ID=36 to CPU0
-                     // config_int(44, 1); // UART0 ID=44 to CPU1
+  if (cpu_id() == 0)
+  {
+    Machine::enable_GIC();
+    Machine::enable_gic_processor_interface();
+    int *apAddr = (int *)0x10000030; // SYS_FLAGSSET register
+    *apAddr = (int)0x10000;          // all APs execute from 0x10000
+    Machine::send_sgi(0x00, 0x0f, 0x01);
+  }
 }
 __END_SYS
 
